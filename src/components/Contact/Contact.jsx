01da16045613ee052aacebaf2b62e36e96cd2c2a
anton41k <anton12petrenko@gmail.com>
2021-10-20 13:26:59 +0300

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import toast from 'react-hot-toast';
 import { useDeleteContactMutation } from "../../services/contactApi";
 import css from "./Contact.module.css";
 
-const Contact = ({ id, name, number, addNameContact }) => {
+const Contact = ({ id, name, number, addNameContact, optionFirstLetter }) => {
   const [deleteContact, { isLoading: isDeleting }] = useDeleteContactMutation();
   if (isDeleting) {
     toast.success(`Contact ${name} is delete!!!`);
@@ -13,6 +13,7 @@ const Contact = ({ id, name, number, addNameContact }) => {
 
   return (
     <li className={`${addNameContact === name ? `${css.contact_item_add}` : `${css.contact_item_h}`} ${css.contact_item}`}>
+      {optionFirstLetter && <span className={css.first_letter} >{optionFirstLetter.firstLetter}</span>}
       <span className={css.contact_name}>{name}</span>
       <span className={css.contact_number}>{number}</span>
 
